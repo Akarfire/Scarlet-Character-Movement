4b9c1dad8b5c79c7736d8f6df338f790ca9db420
K <Akarfire@gmail.com>
2026-03-04 14:55:46 +0300

    [DEV] RollingLayer implemented

---
+++ Source/ScarletCharacterMovement/Private/SCM_RollingLayer.cpp
@@ -0,0 +1,147 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCM_RollingLayer.h"
+
+
+// ROLLING
+
+// Registering parameters
+void USCM_Rolling::SetupParameters_Implementation()
+{
+	UScarletMovementComponent* SM = GetScarletMovement();
+
+	SM->RegisterFloatParameter("RollBoost", RollBoost, true, this, "OnParameterValueChanged");
+	SM->RegisterFloatParameter("RollDelay", RollDelay, true, this, "OnParameterValueChanged");
+	SM->RegisterFloatParameter("RollDuration", RollDuration, true, this, "OnParameterValueChanged");
+	SM->RegisterFloatParameter("RollCooldown", RollCooldown, true, this, "OnParameterValueChanged");
+
+	SM->RegisterBoolParameter("OrientRotationToMovement_Rolling", OrientRotationToMovement, true, this, "OnParameterValueChanged");
+	SM->RegisterBoolParameter("OrientRotationToMovementWhenAiming_Rolling", OrientRotationToMovementWhenAiming, true, this, "OnParameterValueChanged");
+
+	// Timers
+	USTT_TimerController* TC = SM->GetTimerController();
+
+	TC->CreateTimer("RollDelay", RollDelay, false, false);
+	TC->SubscribeToTimer("RollDelay", this, "OnTimerIsOver");
+
+	TC->CreateTimer("RollDuration", RollDuration, false, false);
+	TC->SubscribeToTimer("RollDuration", this, "OnTimerIsOver");
+
+	TC->CreateTimer("RollCooldown", RollCooldown, false, false);
+	TC->SubscribeToTimer("RollCooldown", this, "OnTimerIsOver");
+
+	// Dynamic gates
+	if (SM->IsDynamicGateValid("CanJump"))
+		SM->SetDynamicGateNamedValue("CanJump", "NotRolling", true);
+
+	if (SM->IsDynamicGateValid("CanRoll"))
+		SM->SetDynamicGateNamedValue("CanRoll", "RollCooldown", true);
+
+	if (SM->IsDynamicGateValid("StopRoll"))
+		SM->SetDynamicGateNamedValue("StopRoll", "RollDuration", false);
+}
+
+// Called when any parameter value is changed
+void USCM_Rolling::OnParameterValueChanged(const FName& ParameterName)
+{
+	if (ParameterName == "RollBoost")
+		RollBoost = GetScarletMovement()->GetFloatParameterValue("RollBoost");
+
+	else if (ParameterName == "RollDelay")
+	{
+		RollDelay = GetScarletMovement()->GetFloatParameterValue("RollDelay");
+		GetScarletMovement()->GetTimerController()->ChangeTimerLength("RollDelay", RollDelay);
+	}
+
+	else if (ParameterName == "RollDuration")
+	{
+		RollDuration = GetScarletMovement()->GetFloatParameterValue("RollDuration");
+		GetScarletMovement()->GetTimerController()->ChangeTimerLength("RollDuration", RollDuration);
+	}
+
+	else if (ParameterName == "RollCooldown")
+	{
+		RollCooldown = GetScarletMovement()->GetFloatParameterValue("RollCooldown");
+		GetScarletMovement()->GetTimerController()->ChangeTimerLength("RollCooldown", RollCooldown);
+	}
+
+	else if (ParameterName == "OrientRotationToMovement_Rolling")
+		OrientRotationToMovement = GetScarletMovement()->GetBoolParameterValue("OrientRotationToMovement_Rolling");
+
+	else if (ParameterName == "OrientRotationToMovementWhenAiming_Rolling")
+		OrientRotationToMovementWhenAiming = GetScarletMovement()->GetBoolParameterValue("OrientRotationToMovementWhenAiming_Rolling");
+}
+
+void USCM_Rolling::OnTimerIsOver(FName TimerName)
+{
+	if (TimerName == "RollDelay") { StartRoll(); }
+
+	else if (TimerName == "RollDuration") 
+	{
+		if (GetScarletMovement()->IsDynamicGateValid("StopRoll"))
+			GetScarletMovement()->SetDynamicGateNamedValue("StopRoll", "RollDuration", true);
+	}
+
+	else if (TimerName == "RollCooldown")
+	{
+		if (GetScarletMovement()->IsDynamicGateValid("CanRoll"))
+			GetScarletMovement()->SetDynamicGateNamedValue("CanRoll", "RollCooldown", true);
+	}
+
+}
+
+void USCM_Rolling::StartRoll()
+{
+	FVector BoostVector = GetScarletMovement()->GetRawMovementInputVector() * RollBoost;
+	GetCharacterMovement()->GetCharacterOwner()->LaunchCharacter(BoostVector, false, false);
+
+	// Character rotation
+	FRotator NewRotation = GetScarletMovement()->GetRawMovementInputVector().Rotation();
+	GetCharacterMovement()->GetCharacterOwner()->SetActorRotation(NewRotation);
+
+	// RollDuration -> ... Timer ... -> StopRoll -> ExitState()
+	GetScarletMovement()->GetTimerController()->StartTimer("RollDuration");
+}
+
+void USCM_Rolling::EnterState_Implementation()
+{
+	UCharacterMovementComponent* CM = GetCharacterMovement();
+	CM->SetMovementMode(EMovementMode::MOVE_Walking);
+	CachedGroundFriction = CM->GroundFriction;
+	CM->GroundFriction = 0.f;
+
+	// Dynamic gates
+	if (GetScarletMovement()->IsDynamicGateValid("CanJump"))
+		GetScarletMovement()->SetDynamicGateNamedValue("CanJump", "NotRolling", false);
+
+	if (GetScarletMovement()->IsDynamicGateValid("StopRoll"))
+		GetScarletMovement()->SetDynamicGateNamedValue("StopRoll", "RollDuration", false);
+
+	// Start rolling
+	// RollDelay -> ... Timer ... -> StartRoll();
+	GetScarletMovement()->GetTimerController()->StartTimer("RollDelay");
+}
+
+void USCM_Rolling::ExitState_Implementation()
+{
+	GetCharacterMovement()->GroundFriction = CachedGroundFriction;
+
+	// Dynamic gates
+	if (GetScarletMovement()->IsDynamicGateValid("CanJump"))
+		GetScarletMovement()->SetDynamicGateNamedValue("CanJump", "NotRolling", true);
+
+	if (GetScarletMovement()->IsDynamicGateValid("CanRoll"))
+		GetScarletMovement()->SetDynamicGateNamedValue("CanRoll", "RollCooldown", false);
+
+	GetScarletMovement()->GetTimerController()->StartTimer("RollCooldown");
+}
+
+void USCM_Rolling::UpdateState_Implementation(float DeltaTime)
+{
+	// Orient Rotation To Movement
+	bool IsAiming = GetScarletMovement()->GetBoolInputValue("Aim");
+
+	GetCharacterMovement()->bOrientRotationToMovement = IsAiming ? OrientRotationToMovementWhenAiming : OrientRotationToMovement;
+	GetScarletMovement()->GetCharacterMovementComponent()->GetCharacterOwner()->bUseControllerRotationYaw = !GetCharacterMovement()->bOrientRotationToMovement;
+}
---
+++ Source/ScarletCharacterMovement/Public/SCM_RollingLayer.h
@@ -0,0 +1,151 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SCM_MovementStateMachine.h"
+#include "SCM_MovementStateBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+#include "SCM_LowerLevelState.h"
+#include "SCM_RollingLayer.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ESCM_RollingLayerStates : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Rolling UMETA(DisplayName = "Rolling"),
+	LowerLevel UMETA(DisplayName = "LowerLevel")
+};
+
+// STATES
+
+/**
+ * Rolling state
+ */
+UCLASS()
+class SCARLETCHARACTERMOVEMENT_API USCM_Rolling : public USCM_MovementStateBase
+{
+	GENERATED_BODY()
+
+protected:
+	float CachedGroundFriction = 0.f;
+
+protected:
+
+	// Parameters
+	float RollBoost = 1500.f;
+	float RollDelay = 0.1f;
+	float RollDuration = 0.4f;
+	float RollCooldown = 0.25;
+	bool OrientRotationToMovement = true;
+	bool OrientRotationToMovementWhenAiming = false;
+
+protected:
+
+	void StartRoll();
+
+public:
+
+	// Constructor
+	USCM_Rolling() {}
+
+	// Called when any parameter value is changed
+	UFUNCTION()
+	void OnParameterValueChanged(const FName& ParameterName);
+
+	UFUNCTION()
+	void OnTimerIsOver(FName TimerName);
+
+	// Registering parameters
+	virtual void SetupParameters_Implementation() override;
+
+	// Called when the state is made active
+	virtual void EnterState_Implementation() override;
+
+	// Called once the active state is changed to a different one
+	virtual void ExitState_Implementation() override;
+
+	// Called every state machine update when the state is active
+	virtual void UpdateState_Implementation(float DeltaTime) override;
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class SCARLETCHARACTERMOVEMENT_API USCM_RollingLayer : public USCM_MovementStateMachine
+{
+	GENERATED_BODY()
+
+protected:
+
+public:
+
+	// Constructor
+	USCM_RollingLayer() { StateDisplayNames = { "None", "Rolling", "LowerLevel" }; }
+
+	// Called when the state machine is initialized (after the main init)
+	virtual void OnInitStateMachine_Implementation() override
+	{
+		// Registering States
+		AddNewState((uint8)ESCM_RollingLayerStates::Rolling, USCM_Rolling::StaticClass());
+		AddNewState((uint8)ESCM_RollingLayerStates::LowerLevel, USCM_LowerLayerState::StaticClass());
+
+		//Alternatively you can use automatic Transition Registration:
+		AutoTransitionRegistration(StateDisplayNames, "Condition_", "_");
+
+		// Set Default State
+		ForceCallStateTransition((uint8)ESCM_RollingLayerStates::LowerLevel);
+	}
+
+	// Called every time the state machine is updated (after the main update)
+	virtual void OnUpdateStateMachine_Implementation(float DeltaTime) override
+	{
+		Super::OnUpdateStateMachine_Implementation(DeltaTime);
+	}
+
+	// Called every time the state is changed
+	virtual void OnStateChangedInternal_Implementation(uint8 OldState, uint8 NewState) {}
+
+	// Returns a pointer to the state that contains the lower level of the movement state machine stack
+	virtual USSM_NestedStateMachineState* GetLowerLayerContainerState_Implementation() { return Cast<USSM_NestedStateMachineState>(GetState((uint8)ESCM_RollingLayerStates::LowerLevel)); }
+
+	// Needed to register state-machine level movement parameters (for state transition conditions)
+	virtual void OnSetupParameters_Implementation()
+	{
+		// Register control parameters here
+		// ...
+
+		// Dynamic gates
+		GetScarletMovement()->RegisterDynamicGate("CanRoll", ESCM_DynamicGateRule::And, true);
+		GetScarletMovement()->RegisterDynamicGate("StopRoll", ESCM_DynamicGateRule::Or, false);
+		// ...
+
+		// Timers
+		// ...
+	}
+
+	UFUNCTION()
+	void OnParameterValueChanged(const FName& ParameterName) {}
+
+	UFUNCTION()
+	void OnTimerIsOver(FName InTimerName) {}
+
+	// TRANSITION CONDITIONS
+	// Must be a UFUNCTION, will not work otherwise, and no, it will not crash, you will just be stuck there with no real signs of errors, so yeah
+
+	// Condition for transition from state One to state Two
+	UFUNCTION()
+	bool Condition_LowerLevel_Rolling() { return	GetScarletMovement()->GetBoolInputValue("Roll")
+												 && GetScarletMovement()->GetDynamicGateValue("CanRoll")
+												 && GetScarletMovement()->GetRawMovementInputVector() != FVector::ZeroVector; }
+
+	UFUNCTION()
+	bool Condition_Rolling_LowerLevel() { return GetScarletMovement()->GetDynamicGateValue("StopRoll"); }
+
+	// ...
+
+};
